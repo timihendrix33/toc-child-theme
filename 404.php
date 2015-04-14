1cc456afcc7a240c94cfb7a9ea17332b11fa9c07
Timothy.Stevenson <t.stevenson33@gmail.com>
2015-04-14 18:32:50 -0400

    changing 404 page

---
+++ 404.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Template Name: 404
+ */
+ get_header(); ?>
+ 
+
+<div class="entry-content">
+    <h1>404!</h1>
+    Look like something went wrong! The page you were looking for is not here <br> 
+    Check on <a href="http://archive.thoughtsoncloud.com">archive.thoughtsoncloud.com</a> or try a search:
+    <div class="search-form">
+        <?php get_search_form(); ?>
+        <i class="icon-search"></i>
+    </div>
+</div>
+<div id="primary"> 
+    <div id="content"> 
+        <div id="side-content">
+            <?php get_template_part( 'navigation' ); ?>
+        </div>
+        <div id="main-content">
+            <div id="main-content-inner">
+                <?php 
+                    $page_var = 'paged';
+                    if( is_404() ) {
+                        $page_var = 'page';
+                    }
+                    $paged = get_query_var( $page_var ) ? get_query_var( $page_var ) : 1;
+                    $featuredtag = get_tag_id_by_name('featured');
+					$videocat = get_cat_ID( 'video' );
+					$eventcat = get_cat_ID( 'events' );
+		            $args=array(
+					'tag__not_in' => array($featuredtag),
+					'post_type=post&paged='.$paged,
+					'category__not_in' => array($eventcat,$videocat),
+					'paged' => $paged
+					);
+					$wp_query = new WP_Query($args);
+                ?>
+                <?php if ( $wp_query->have_posts() ) : ?>
+                    <?php while ( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
+                        <?php get_template_part( 'content', get_post_format() ); ?>
+                    <?php endwhile; ?>
+                    <?php dw_paging_nav(); ?>
+                <?php else : ?>
+                    <?php get_template_part( 'content', 'none' ); ?>
+                <?php endif; ?>
+                <?php wp_reset_postdata(); ?>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="secondary">
+    <div class="secondary-inner">
+        <?php get_sidebar(); ?>
+    </div>
+</div>
+<?php get_footer(); ?>
+
